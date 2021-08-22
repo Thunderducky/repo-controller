bd10751c422915244fa8bd29b72ba613473e7abc
Eric Scott <Thunderducky@users.noreply.github.com>
2021-08-22 18:48:01 -0500

    Initial commit

---
+++ dist/bundle.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+function component() {
+    var element = document.createElement('div');
+    // Lodash, currently included via a script, is required for this line to work
+    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.textContent = "Hello World";
+    return element;
+}
+document.body.appendChild(component());
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90eXBlc2NyaXB0LWdhbWUvLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLFNBQVMsU0FBUztJQUNkLElBQU0sT0FBTyxHQUFHLFFBQVEsQ0FBQyxhQUFhLENBQUMsS0FBSyxDQUFDLENBQUM7SUFFOUMsNkVBQTZFO0lBQzdFLHdEQUF3RDtJQUN4RCxPQUFPLENBQUMsV0FBVyxHQUFHLGFBQWE7SUFDbkMsT0FBTyxPQUFPLENBQUM7QUFDbkIsQ0FBQztBQUVELFFBQVEsQ0FBQyxJQUFJLENBQUMsV0FBVyxDQUFDLFNBQVMsRUFBRSxDQUFDLENBQUMiLCJmaWxlIjoiYnVuZGxlLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZnVuY3Rpb24gY29tcG9uZW50KCkge1xuICAgIGNvbnN0IGVsZW1lbnQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcblxuICAgIC8vIExvZGFzaCwgY3VycmVudGx5IGluY2x1ZGVkIHZpYSBhIHNjcmlwdCwgaXMgcmVxdWlyZWQgZm9yIHRoaXMgbGluZSB0byB3b3JrXG4gICAgLy9lbGVtZW50LmlubmVySFRNTCA9IF8uam9pbihbJ0hlbGxvJywgJ3dlYnBhY2snXSwgJyAnKTtcbiAgICBlbGVtZW50LnRleHRDb250ZW50ID0gXCJIZWxsbyBXb3JsZFwiXG4gICAgcmV0dXJuIGVsZW1lbnQ7XG59XG5cbmRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoY29tcG9uZW50KCkpOyJdLCJzb3VyY2VSb290IjoiIn0=
